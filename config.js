80e796893290aff7e22b34bdd346a75e70c357df
anhtue306 <92808552+anhtue306@users.noreply.github.com>
2021-11-30 10:59:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ const CONFIG = {
     tớ thích cậu từ lâu mà đúng không ? :D `,
     btnIntro: 'hihi',
     title: 'Tớ chỉ mún nói là :',
-    desc: 'Tớ thích cậu GẤT GẤT là nhiều, nếu được cậu có thể mở lòng cho tớ 1 chút đk :>>  ',
+    desc: 'Tớ thích cậu rất là nhiều, cậu có thể mở lòng cho tớ 1 chút ít đk :<  ',
     btnYes: 'Cũm được , Oke',
     btnNo: 'Không,No hope :<<',
-    question:'có gì muốn nói lại tao khom ',
+    question:'có gì muốn nói lại tớ khom ',
     btnReply: 'Gửi cậu Tuệ <3',
     reply: 'I <3 U ',
-    mess: 'T biết mà 🥰. cảm ơn nhiều nhiều nhiều 😘😘',
-    messDesc: 'Túm lại là tớ chỉ muốn nói với cậu là tớ yêu cậu dị hoi ',
+    mess: 'Tớ biết mà 🥰. cảm ơn nhiều nhiều nhiều 😘😘',
+    messDesc: 'Túm lại là tớ chỉ muốn nói với cậu là tớ yêu cậu bự lắm  ',
     btnAccept: 'cảm ơn nhoa <3',
     messLink: 'https://media0.giphy.com/media/xlNDtecvg6zLrXvOaa/giphy.gif?cid=ecf05e47nexy6xz7hcsjd22xo3emufzcwl52e1bajdgvii1a&rid=giphy.gif&ct=g/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
