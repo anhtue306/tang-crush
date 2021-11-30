d14f18d3066dcc82ff8033a945076521ecd8bc41
anhtue306 <92808552+anhtue306@users.noreply.github.com>
2021-11-30 10:45:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Bảo Ngọc ơi !',
-    introDesc: `chắc m cũm biết là.... 
-    t thích m từ lâu mà đúng không ? :D `,
+    introDesc: `chắc cậu cũm biết là.... 
+    tớ thích cậu từ lâu mà đúng không ? :D `,
     btnIntro: 'hihi',
-    title: 'Tao- Nguyễn Anh Tuê- chỉ mún nói là :',
-    desc: 'Tao thích m GẤT GẤT là nhiều, nếu được m có thể mở lòng cho t 1 chút đk :>>  ',
+    title: 'Tớ chỉ mún nói là :',
+    desc: 'Tớ thích cậu GẤT GẤT là nhiều, nếu được cậu có thể mở lòng cho tớ 1 chút đk :>>  ',
     btnYes: 'Cũm được , Oke',
     btnNo: 'Không,No hope :<<',
     question:'có gì muốn nói lại tao khom ',
-    btnReply: 'Gửi bạn Tuệ <3',
+    btnReply: 'Gửi cậu Tuệ <3',
     reply: 'I <3 U ',
     mess: 'T biết mà 🥰. cảm ơn nhiều nhiều nhiều 😘😘',
-    messDesc: 'Túm lại là t chỉ muốn nói với m dị hoi.',
+    messDesc: 'Túm lại là tớ chỉ muốn nói với cậu là tớ yêu cậu dị hoi ',
     btnAccept: 'cảm ơn nhoa <3',
     messLink: 'https://media0.giphy.com/media/xlNDtecvg6zLrXvOaa/giphy.gif?cid=ecf05e47nexy6xz7hcsjd22xo3emufzcwl52e1bajdgvii1a&rid=giphy.gif&ct=g/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
